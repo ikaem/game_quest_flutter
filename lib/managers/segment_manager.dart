24ff79068ad0d50df8aed845febe520660cf2648
Karlo M <karlo.marinovic@decode.agency>
2023-11-15 15:54:33 +0100

    populate scene with segments

--- lib/managers/segment_manager.dart
+++ lib/managers/segment_manager.dart
@@ -1,4 +1,8 @@
 import 'package:flame/components.dart';
+import 'package:game/actors/water_enemy.dart';
+import 'package:game/objects/ground_block.dart';
+import 'package:game/objects/platform_block.dart';
+import 'package:game/objects/star.dart';
 
 class Block {
 
@@ -12,7 +16,106 @@ class Block {
 
 // segments will allow us to assemble world - envbiroments
 // segments are made out of Blocks
-  final segments = [
-    segment0,
-  ];
-  final segment0 = [];
+// segments are units that the current scene consists of
+// scene always consists of at lest three segments (5 in this case)
+final segments = [
+  segment0,
+  segment1,
+  segment2,
+  segment3,
+  segment4,
+];
+final segment0 = [
+  Block(Vector2(0, 0), GroundBlock),
+  Block(Vector2(1, 0), GroundBlock),
+  Block(Vector2(2, 0), GroundBlock),
+  Block(Vector2(3, 0), GroundBlock),
+  Block(Vector2(4, 0), GroundBlock),
+  Block(Vector2(5, 0), GroundBlock),
+  Block(Vector2(5, 1), WaterEnemy),
+  Block(Vector2(5, 3), PlatformBlock),
+  Block(Vector2(6, 0), GroundBlock),
+  Block(Vector2(6, 3), PlatformBlock),
+  Block(Vector2(7, 0), GroundBlock),
+  Block(Vector2(7, 3), PlatformBlock),
+  Block(Vector2(8, 0), GroundBlock),
+  Block(Vector2(8, 3), PlatformBlock),
+  Block(Vector2(9, 0), GroundBlock),
+];
+
+final segment1 = [
+  Block(Vector2(0, 0), GroundBlock),
+  Block(Vector2(1, 0), GroundBlock),
+  Block(Vector2(1, 1), PlatformBlock),
+  Block(Vector2(1, 2), PlatformBlock),
+  Block(Vector2(1, 3), PlatformBlock),
+  Block(Vector2(2, 6), PlatformBlock),
+  Block(Vector2(3, 6), PlatformBlock),
+  Block(Vector2(6, 5), PlatformBlock),
+  Block(Vector2(7, 5), PlatformBlock),
+  Block(Vector2(7, 7), Star),
+  Block(Vector2(8, 0), GroundBlock),
+  Block(Vector2(8, 1), PlatformBlock),
+  Block(Vector2(8, 5), PlatformBlock),
+  Block(Vector2(8, 6), WaterEnemy),
+  Block(Vector2(9, 0), GroundBlock),
+];
+
+final segment2 = [
+  Block(Vector2(0, 0), GroundBlock),
+  Block(Vector2(1, 0), GroundBlock),
+  Block(Vector2(2, 0), GroundBlock),
+  Block(Vector2(3, 0), GroundBlock),
+  Block(Vector2(3, 3), PlatformBlock),
+  Block(Vector2(4, 0), GroundBlock),
+  Block(Vector2(4, 3), PlatformBlock),
+  Block(Vector2(5, 0), GroundBlock),
+  Block(Vector2(5, 3), PlatformBlock),
+  Block(Vector2(5, 4), WaterEnemy),
+  Block(Vector2(6, 0), GroundBlock),
+  Block(Vector2(6, 3), PlatformBlock),
+  Block(Vector2(6, 4), PlatformBlock),
+  Block(Vector2(6, 5), PlatformBlock),
+  Block(Vector2(6, 7), Star),
+  Block(Vector2(7, 0), GroundBlock),
+  Block(Vector2(8, 0), GroundBlock),
+  Block(Vector2(9, 0), GroundBlock),
+];
+
+final segment3 = [
+  Block(Vector2(0, 0), GroundBlock),
+  Block(Vector2(1, 0), GroundBlock),
+  Block(Vector2(1, 1), WaterEnemy),
+  Block(Vector2(2, 0), GroundBlock),
+  Block(Vector2(2, 1), PlatformBlock),
+  Block(Vector2(2, 2), PlatformBlock),
+  Block(Vector2(4, 4), PlatformBlock),
+  Block(Vector2(6, 6), PlatformBlock),
+  Block(Vector2(7, 0), GroundBlock),
+  Block(Vector2(7, 1), PlatformBlock),
+  Block(Vector2(8, 0), GroundBlock),
+  Block(Vector2(8, 8), Star),
+  Block(Vector2(9, 0), GroundBlock),
+];
+
+final segment4 = [
+  Block(Vector2(0, 0), GroundBlock),
+  Block(Vector2(1, 0), GroundBlock),
+  Block(Vector2(2, 0), GroundBlock),
+  Block(Vector2(2, 3), PlatformBlock),
+  Block(Vector2(3, 0), GroundBlock),
+  Block(Vector2(3, 1), WaterEnemy),
+  Block(Vector2(3, 3), PlatformBlock),
+  Block(Vector2(4, 0), GroundBlock),
+  Block(Vector2(5, 0), GroundBlock),
+  Block(Vector2(5, 5), PlatformBlock),
+  Block(Vector2(6, 0), GroundBlock),
+  Block(Vector2(6, 5), PlatformBlock),
+  Block(Vector2(6, 7), Star),
+  Block(Vector2(7, 0), GroundBlock),
+  Block(Vector2(8, 0), GroundBlock),
+  Block(Vector2(8, 3), PlatformBlock),
+  Block(Vector2(9, 0), GroundBlock),
+  Block(Vector2(9, 1), WaterEnemy),
+  Block(Vector2(9, 3), PlatformBlock),
+];
