5f164995c99bfa37c0558676ca76d9f6fc34d7e0
Karlo M <karlo.marinovic@decode.agency>
2023-11-15 16:09:48 +0100

    platform block class

--- lib/objects/platform_block.dart
+++ lib/objects/platform_block.dart
@@ -1,1 +1,30 @@
-class PlatformBlock {}
+/*
+* idea is
+* - get srpite to be displayed - place it in the correct position
+* - removbe the sprite as they are off the screen
+* */
+
+import 'package:flame/components.dart';
+import 'package:game/game_quest.dart';
+
+class PlatformBlock extends SpriteComponent with HasGameRef<GameQuestGame>{
+
+  final Vector2 gridPosition;
+  double xOffset;
+
+  PlatformBlock({
+    required this.gridPosition,
+    required this.xOffset,
+  }) : super(size: Vector2.all(64), anchor: Anchor.bottomLeft);
+
+  // note empty onLoad and update methods
+  @override
+  void onLoad() {
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+
+}
