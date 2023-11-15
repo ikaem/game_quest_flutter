1858248e2ffabcba902fc4584ad0438e9ea99a05
Karlo M <karlo.marinovic@decode.agency>
2023-11-15 15:58:59 +0100

    load semgets into game

--- lib/game_quest.dart
+++ lib/game_quest.dart
@@ -2,6 +2,11 @@ import 'package:flame/camera.dart';
 import 'package:flame/game.dart';
 import 'package:flame/widgets.dart';
 import 'package:game/actors/ember.dart';
+import 'package:game/actors/water_enemy.dart';
+import 'package:game/managers/segment_manager.dart';
+import 'package:game/objects/ground_block.dart';
+import 'package:game/objects/platform_block.dart';
+import 'package:game/objects/star.dart';
 
 class GameQuestGame extends FlameGame {
   GameQuestGame();
@@ -12,7 +17,7 @@ class GameQuestGame extends FlameGame {
   late final CameraComponent cameraComponent;
 
   @override
-  Future<void> onLoad() async{
+  Future<void> onLoad() async {
     // this uses Flame's build in caching system - load images only once - access as many times as needed
     // it takes them from assets/images
     await images.loadAll([
@@ -25,7 +30,6 @@ class GameQuestGame extends FlameGame {
       'water_enemy.png',
     ]);
 
-
     cameraComponent = CameraComponent(world: world);
     // for some reason, we assume that camera position is top left
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
@@ -37,6 +41,23 @@ class GameQuestGame extends FlameGame {
     // now the chracter should be present on the screen
     _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
     world.add(_ember);
+  }
 
+  // this is to load blocks into the world
+  // we give inted for segments list, and we loop through that segment - will populate the wrold
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    final segment = segments[segmentIndex];
+    for (final block in segment) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+      }
+    }
   }
 }
